881327ccc53ba7cf036c11f28edd306bbd0f4358
Devran <77250053+DEVR4N@users.noreply.github.com>
2024-06-21 11:59:37 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
             <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
                 <div className="flex flex-col justify-start items-start gap-6">
-                    <Image src="/pink-girl-logo-cut.png" alt="Car Hub Logo" width={118} height={18} className="object-contain"/>
+                    <Image src="/pink-girl-logo-cut.png" alt="Pink Logo" width={118} height={18} className="object-contain"/>
                     <p className="text-base">2024 Pink Rent a Car. <br/> Tüm hakları saklıdır &copy;</p>
                 </div>
             </div>
